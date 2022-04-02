c31391a9bd87b0ecd9caf7846d50a8082518ad2b
bgenia <bgenia@student.21-school.ru>
2022-04-02 16:16:21 +0300

    Fix whatever

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -1,25 +1,41 @@
 #pragma once
 
 template<typename T>
-void swap(T a, T b)
+void swap(T& a, T& b)
 {
-    T temp = a;
+    T temp(a);
     a = b;
     b = temp;
 }
 
 template<typename T>
-T min(T a, T b)
+const T& min(const T& a, const T& b)
 {
-    if (a > b)
+    if (a >= b)
         return b;
     return a;
 }
 
 template<typename T>
-T max(T a, T b)
+T& min(T& a, T& b)
 {
-    if (a > b)
-        return a;
-    return b;
+    if (a >= b)
+        return b;
+    return a;
+}
+
+template<typename T>
+const T& max(const T& a, const T& b)
+{
+    if (a <= b)
+        return b;
+    return a;
+}
+
+template<typename T>
+T& max(T& a, T& b)
+{
+    if (a <= b)
+        return b;
+    return a;
 }
